05079d7a8ca446cdef6f611a40cdf7b233a477ff
matnaryo <rohmat.sunaryo@gmail.com>
2026-03-28 22:20:12 +0700

    edit upload_to_drive.py

--- upload_to_gdrive.py
+++ upload_to_gdrive.py
@@ -6,29 +6,30 @@ from upload.uploader import upload_model, upload_file
 
 
 def main():
-    root_folder_id = os.environ["GDRIVE_FOLDER_ID"]
+    # tentukan direktori di gdrive sesuai Drive ID
+    root_dir = os.environ["GDRIVE_FOLDER_ID"]
 
+    # ambil kredential OAuth2 untuk bisa login ke akun gdrive
     credentials = get_credentials()
     service = get_drive_service(credentials)
 
-    # buat folder baru tiap run
-    folder_name = generate_folder_name()
-    run_folder_id = create_folder(service, folder_name, root_folder_id)
-    print("Folder created:", folder_name)
+    # buat fdirektori utama app dengan nama unik
+    app_dir_name = generate_folder_name()
+    app_dir = create_folder(service, app_dir_name, root_dir)
 
-    # buat folder model di dalam run folder
-    model_folder_id = create_folder(service, "model", run_folder_id)
-    print("Model folder created:", "model")
-    eda_folder_id = create_folder(service, "eda", run_folder_id)
-    print("Model folder created:", "eda")
+    # buat direktori untuk model dan EDA di dalam direktori app utama
+    model_dir = create_folder(service, "model", app_dir)
+    eda_dir = create_folder(service, "eda", app_dir)
 
-    local_model_folder = "preprocessing/model"
-    local_eda_folder = "preprocessing/eda"
+    # tentukan direktori local(runner) dari model dan EDA
+    local_model_dir = "preprocessing/model"
+    local_eda_dir = "preprocessing/eda"
 
-    # upload model folder
-    upload_model(service, local_model_folder, model_folder_id)
-    upload_model(service, local_eda_folder, model_folder_id)
-    # upload csv
+    # upload direktori local ke gdrive sesuai kriteria
+    upload_model(service, local_model_dir, model_dir)
+    upload_model(service, local_eda_dir, model_dir)
+
+    # seleksi file csv dari local runner
     model_csv_files = [
         "preprocessing/train_processed.csv",
         "preprocessing/test_processed.csv",
@@ -37,10 +38,11 @@ def main():
         "preprocessing/eda/eda_summary.txt",
     ]
 
+    # upload file csv sesuai kriteria
     for file in model_csv_files:
-        upload_file(service, file, run_folder_id)
+        upload_file(service, file, app_dir)
     for file in eda_csv_files:
-        upload_file(service, file, eda_folder_id)
+        upload_file(service, file, eda_dir)
 
 
 if __name__ == "__main__":
